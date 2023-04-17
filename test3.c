41b5b9025c50dc2725752db55659a61d64dd00d2
MorbidMachinery <morbidmachinery@MorbidMachinery>
2023-04-17 16:36:48 +0530

    Initialized c program
    reset author

---
+++ test3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<readline/history.h>
+#include<readline/readline.h>
+
+int main() {
+    HISTORY_STATE* history_data = history_get_history_state();
+    int base_index = history_base;
+    int history_length_var = history_get_history_length(history_data);
+    printf("Base index = %d\n", base_index);
+    printf("History length = %d\n", history_length_var); 
+    return 0;
+}
